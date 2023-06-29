b30d5e39375fd6a3945ab8e2df6d08d0dfb04da0
Vadim Tsarev <74840902+Vadimjkeee@users.noreply.github.com>
2023-06-30 01:44:15 +0300

    Update getText.js
    attempt detour CORS

--- src/assets/getText.js
+++ src/assets/getText.js
@@ -13,21 +13,4 @@
 // script.src = "http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=ru&jsonp=handleResponse";
 // document.head.appendChild(script);
 
-
-function handleResponse(response) {
-    if (
-      Object.prototype.hasOwnProperty.call(response, "quoteText") &&
-      Object.prototype.hasOwnProperty.call(response, "quoteAuthor")
-    ) {
-      const quote = response.quoteText;
-      const author = response.quoteAuthor;
-      console.log("Цитата: " + quote);
-      console.log("Автор: " + author);
-      this.apiText = quote;
-    } else {
-      console.log("Не удалось получить цитату.");
-    }
-  }
-  
-  window.handleResponse = handleResponse;
   
